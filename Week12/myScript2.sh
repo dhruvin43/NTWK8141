76091800244cce6e47cbfb43f296fa43950f4de2
dhruvin43 <dhruvinrabadiya43@gmail.com>
2024-07-24 21:18:33 -0400

    Adding bash script

---
+++ Week12/myScript2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#A script that uses conditional expressions and loops 
+count=10
+until [ $count -le 0 ]
+do
+	echo the loop count is $count
+		count=$[ $count - 1 ]
+	done
+#
+if [ $count -eq 0 ]
+then
+	echo "Count is set to zero: $count"
+else
+	echo "Count is not set to zero: $count"
+fi
+exit
+
